016df14892a534f6c830a60bd0be8f02dda51aa7
Vihan Lakshman <vihan@Vihans-MacBook-Pro.local>
2021-08-24 21:40:27 -0700

    Add md5 hash

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// Implementation of count-min sketch data structure
+
+// Struct that has two things
+
+// consume(item) -> add to the respective counters
+
+// query(item) -> return the (approximate) count of item
+
+use md5;
+
+
 fn main() {
-    println!("Hello, world!");
+	let digest = md5::compute(b"abcdefghijklmnopqrstuvwxyz");
+    println!("The hash is {:x}", digest);
 }
+
+
