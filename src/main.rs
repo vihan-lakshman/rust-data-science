51a8b3c3b4d98069cb42864a6d49fbb4ff83d86f
arichard <arichard@cs.stanford.edu>
2021-08-24 17:15:00 -0700

    Basic outline (types) of CountMinSketch

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,37 @@
 
 use md5;
 
-
-fn main() {
-	let digest = md5::compute(b"abcdefghijklmnopqrstuvwxyz");
-    println!("The hash is {:x}", digest);
+pub struct CountMinSketch {
+	hash_tables: [[u8; 256]; 4],
 }
 
+impl CountMinSketch {
+	pub fn new() -> CountMinSketch {
+		CountMinSketch {hash_tables: [[0; 256]; 4]}
+	}
 
+	pub fn consume(&mut self, value: &u8) -> bool {
+		true
+	}
+
+	pub fn query(&self, value: &u8) -> u8 {
+		1
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+    #[test]
+    fn basic_test() {
+    	let inputs = [1,2,3,4,5,6,7,8,9,10];
+    	let mut CMS = CountMinSketch::new();
+    	for item in inputs.iter() {
+    		assert!(CMS.consume(item))
+    	}
+    	for item in inputs.iter() {
+    		assert_eq!(1, CMS.query(item))
+    	}
+    }
+}
